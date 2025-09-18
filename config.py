336f36f30ab2624f9502b4ea3b7386c2467f1100
Devensh22345 <kumardevensh0@gmail.com>
2025-09-18 15:33:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ API_ID = 22207976
 API_HASH = "5c0ad7c48a86afac87630ba28b42560d"
 
 # Main bot token (the bot that manages clones)
-MAIN_BOT_TOKEN = "PUT_YOUR_MAIN_BOT_TOKEN_HERE"
+MAIN_BOT_TOKEN = "7310253875:AAGNAiqUcjwj0xeryJMuhSShPHBEVwP5Cyc"
 
 # MongoDB URI (e.g. MongoDB Atlas)
-MONGO_URI = "mongodb+srv://username:password@cluster.mongodb.net/"
+MONGO_URI = "mongodb+srv://light1:light1@cluster0.tv3clzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
